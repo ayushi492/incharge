ec1505f7d11245ad860a2113669721ea53ed9e3c
ayushi492 <72792489+ayushi492@users.noreply.github.com>
2020-10-22 21:09:21 +0530

    Update DbClass.php

--- DatabaseControl/DbClass.php
+++ DatabaseControl/DbClass.php
@@ -2,7 +2,7 @@
 class Database{
 	private $db;
 	public function connection(){
-		$this->db=mysqli_connect("localhost","root","","incfront");
+		$this->db=mysqli_connect("database-1-instance-1.cq612gkfzj3y.us-east-1.rds.amazonaws.com","incdbadmin","LifeCycle12","incfront");
 		if (mysqli_connect_errno()) {
 			return "Failed to connect";
 			exit();
